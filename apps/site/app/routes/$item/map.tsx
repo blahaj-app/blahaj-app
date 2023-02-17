7bc0acf58bed7380eddaaf4c7f356a07e4b264a3
msa <xcvr48@gmail.com>
2023-02-16 22:08:59 -0800

    fix fouc (maybe?)

--- apps/site/app/routes/$item/map.tsx
+++ apps/site/app/routes/$item/map.tsx
@@ -539,8 +539,7 @@ const Map: FC = () => {
       }}
     >
       <MotionFlex
-        initial={false}
-        animate={{ "--store-basics-height": storeBasicsHeight + "px" } as any}
+        animate={{ "--store-basics-height": storeBasicsHeight ? storeBasicsHeight + "px" : "unset" } as any}
         transition={transition}
         height="100%"
         flexDirection={{ base: "column-reverse", md: "row" }}
@@ -548,7 +547,10 @@ const Map: FC = () => {
         <Sidebar />
         <Box
           width="100%"
-          height={{ base: `calc(100vh - 19.25rem - var(--store-basics-height))`, md: "unset" }}
+          height={{
+            base: storeBasicsHeight ? `calc(100vh - 19.25rem - var(--store-basics-height))` : "calc(100vh - 25rem)",
+            md: "unset",
+          }}
           flexGrow={{ base: "unset", md: "1" }}
           ref={mapContainerRef}
           position="relative"
