383442d366231bd6ea4678eefa43f1b1632c2d7b
msa <xcvr48@gmail.com>
2023-02-19 11:15:21 -0800

    add test emoji

--- apps/site/app/utils/templates.ts
+++ apps/site/app/utils/templates.ts
@@ -1,6 +1,6 @@
 export const mapGlobalMetaTitle = (itemName: string) => `${itemName} Stock Map`;
 export const mapGlobalMetaDescription = (itemName: string) => `Map of ${itemName} stock and restocks around the world.`;
 export const mapStoreMetaTitle = (itemName: string, storeName: string, flag: string) =>
-  `${itemName} @ ${storeName} ${flag}`;
+  `${itemName} @ ${storeName} ${flag} 🍦`;
 export const mapStoreMetaDescription = (itemName: string, storeName: string, countryName: string, flag: string) =>
-  `Stock and restocks of ${itemName} at ${storeName} in ${countryName} ${flag}.`;
+  `Stock and restocks of ${itemName} at ${storeName} in ${countryName} ${flag} 🍦`;
