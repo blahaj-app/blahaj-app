5f425098187b933cbfd4daecf387e7d7871f2537
msa <xcvr48@gmail.com>
2023-02-19 20:25:55 -0800

    test 2

--- apps/site/app/routes/embed/[og.png].tsx
+++ apps/site/app/routes/embed/[og.png].tsx
@@ -6,80 +6,86 @@ import initYoga from "yoga-wasm-web";
 import yogaWasm from "../../bin/yoga-wasm";
 
 export const loader = async ({ params }: LoaderArgs) => {
-  const yoga = await initYoga(yogaWasm);
+  return initYoga(yogaWasm)
+    .then((yoga) => {
+      init(yoga);
 
-  await init(yoga);
+      return fetch("https://t89.s3-us-west-1.amazonaws.com/2023/02/E8cSiKQU/Poppins-Bold.ttf");
+    })
+    .then((font) => {
+      if (!font.ok) {
+        throw new Response("Internal Server Error", { status: 500 });
+      }
 
-  const font = await fetch("https://t89.s3-us-west-1.amazonaws.com/2023/02/E8cSiKQU/Poppins-Bold.ttf");
-  if (!font.ok) {
-    throw new Error("Failed to load font");
-  }
+      return font.arrayBuffer();
+    })
+    .then((font) => {
+      const satori = _satori as typeof SatoriType;
 
-  console.log("Loaded font", font.url);
-
-  const satori = _satori as typeof SatoriType;
-
-  const svg = await satori(
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
-        fontSize: 60,
-        letterSpacing: -2,
-        fontWeight: 700,
-        textAlign: "center",
-      }}
-    >
-      <div
-        style={{
-          backgroundImage: "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
-          backgroundClip: "text",
-          color: "transparent",
-        }}
-      >
-        Develop
-      </div>
-      <div
-        style={{
-          backgroundImage: "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
-          backgroundClip: "text",
-          color: "transparent",
-        }}
-      >
-        Preview
-      </div>
-      <div
-        style={{
-          backgroundImage: "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
-          backgroundClip: "text",
-          color: "transparent",
-        }}
-      >
-        Ship
-      </div>
-    </div>,
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
+      return satori(
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
+            fontSize: 60,
+            letterSpacing: -2,
+            fontWeight: 700,
+            textAlign: "center",
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
+              backgroundClip: "text",
+              color: "transparent",
+            }}
+          >
+            Develop
+          </div>
+          <div
+            style={{
+              backgroundImage: "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
+              backgroundClip: "text",
+              color: "transparent",
+            }}
+          >
+            Preview
+          </div>
+          <div
+            style={{
+              backgroundImage: "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
+              backgroundClip: "text",
+              color: "transparent",
+            }}
+          >
+            Ship
+          </div>
+        </div>,
         {
-          name: "Poppins",
-          data: await font.arrayBuffer(),
-          weight: 700,
-          style: "normal",
+          width: 1200,
+          height: 630,
+          fonts: [
+            {
+              name: "Poppins",
+              data: font,
+              weight: 700,
+              style: "normal",
+            },
+          ],
         },
-      ],
-    },
-  );
-
-  return new Response(svg, {
-    headers: {
-      "Content-Type": "image/svg+xml",
-    },
-  });
+      );
+    })
+    .then(
+      (svg) =>
+        new Response(svg, {
+          headers: {
+            "Content-Type": "image/svg+xml",
+          },
+        }),
+    );
 };
