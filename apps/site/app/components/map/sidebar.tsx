98f731856bbd3a9621d144cda01d38fddf204ffe
repository <xcvr48@gmail.com>
2023-04-07 21:47:34 -0700

    remove sentry test button

--- apps/site/app/components/map/sidebar.tsx
+++ apps/site/app/components/map/sidebar.tsx
@@ -177,13 +177,6 @@ export const Sidebar: FC = () => {
         <Box as={SimpleBar} fontSize="14" maxHeight="100%" autoHide={false}>
           <Box paddingX="6" paddingY="4">
             <Heading size="md">Redesigned & Rebuilt</Heading>
-            <button
-              onClick={() => {
-                throw new Error("Sentry Test");
-              }}
-            >
-              Throw Error
-            </button>
             <p>
               Hey there. Long time no see. I think.
               <br />
