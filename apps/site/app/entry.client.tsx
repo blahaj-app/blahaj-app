b59a84cf284031662cee155e780a3c3ba66f6526
repository <xcvr48@gmail.com>
2023-04-17 02:19:44 -0700

    disable sentry replay masking

--- apps/site/app/entry.client.tsx
+++ apps/site/app/entry.client.tsx
@@ -14,17 +14,15 @@ Sentry.init({
     new Sentry.BrowserTracing({
       routingInstrumentation: Sentry.remixRouterInstrumentation(useEffect, useLocation, useMatches),
     }),
-    new Sentry.Replay(),
+    new Sentry.Replay({
+      maskAllText: false,
+      maskAllInputs: false,
+      blockAllMedia: false,
+    }),
   ],
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.4,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-
-  // ...
 });
 
 const ClientCacheProvider: FC<PropsWithChildren> = ({ children }) => {
